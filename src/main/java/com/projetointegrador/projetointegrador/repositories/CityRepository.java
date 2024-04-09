386f65d76a794ef7b2361f42962ccc95582ad638
samuelt005 <samuel.thomas1109@outlook.com>
2024-04-09 10:28:03 -0300

    Removido warnings

--- src/main/java/com/projetointegrador/projetointegrador/repositories/CityRepository.java
+++ src/main/java/com/projetointegrador/projetointegrador/repositories/CityRepository.java
@@ -1,12 +1,7 @@
 package com.projetointegrador.projetointegrador.repositories;
 
 import com.projetointegrador.projetointegrador.models.City;
-import com.projetointegrador.projetointegrador.projections.CityProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface CityRepository extends JpaRepository<City, Long> {
 }
