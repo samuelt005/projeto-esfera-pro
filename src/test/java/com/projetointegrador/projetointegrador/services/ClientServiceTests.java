c58cf052f765f94fd558cb3d7f296949f608499e
Samuel Thomas <samuel.thomas1109@outlook.com>
2024-04-29 22:09:15 -0300

    Finalização de testes referente a clientes

--- src/test/java/com/projetointegrador/projetointegrador/services/ClientServiceTests.java
+++ src/test/java/com/projetointegrador/projetointegrador/services/ClientServiceTests.java
@@ -1,7 +1,9 @@
 package com.projetointegrador.projetointegrador.services;
 
+import com.projetointegrador.projetointegrador.models.Address;
 import com.projetointegrador.projetointegrador.models.Client;
 import com.projetointegrador.projetointegrador.repositories.ClientRepository;
+import com.projetointegrador.projetointegrador.responses.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -53,27 +55,74 @@ public class ClientServiceTests {
 
     @Test
     void testCreateClient() {
-        // TODO add test
+        Address mockAddress = new Address();
+        Client mockClient = new Client();
+        mockClient.setAddress(mockAddress);
+
+        when(clientRepository.save(mockClient)).thenReturn(mockClient);
+
+        ResponseEntity<?> responseEntity = clientService.createClient(mockClient);
+
+        // Verifica se o cliente foi criado com sucesso e compara o status da request
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertEquals(mockClient, responseEntity.getBody());
     }
 
     @Test
     void testUpdateClient() {
-        // TODO add test
+        Address mockAddress = new Address();
+        mockAddress.setId(1L);
+        Client mockClient = new Client();
+        mockClient.setAddress(mockAddress);
+        mockClient.setId(1L);
+        mockClient.setCpf("10179667025");
+        mockClient.setCnpj("48110821000107");
+
+        when(clientRepository.save(mockClient)).thenReturn(mockClient);
+
+        ResponseEntity<?> responseEntity = clientService.updateClient(mockClient);
+
+        // Verifica se o cliente foi atualizado com sucesso e compara o status da request
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+
+        assertEquals(mockClient, responseEntity.getBody());
     }
 
     @Test
     void testDeleteClient() {
-        // TODO add test
+        Long clientId = 1L;
+        Client mockClient = new Client();
+        mockClient.setId(clientId);
+
+        when(clientRepository.findById(clientId)).thenReturn(Optional.of(mockClient));
+
+        ResponseEntity<?> responseEntity = clientService.deleteClient(clientId);
+
+        // Verifica se o cliente foi desativado com sucesso e compara o status da request
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Response responseBody = (Response) responseEntity.getBody();
+        assertNotNull(responseBody);
+        assertEquals(200, responseBody.getStatus());
+        assertEquals("Cliente inativado.", responseBody.getMessage());
     }
 
     @Test
-    void testValidateClient() {
-        // TODO add test
-    }
+    void testIsAlreadyRegistered() {
+        Client mockClient = new Client();
+        mockClient.setId(1L);
+        mockClient.setCpf("12345678910");
+        mockClient.setCnpj("12345678901234");
 
-    @Test
-    void testIsAlreadyRegisteredisAlreadyRegistered() {
-        // TODO add test
+        when(clientRepository.findByCpf(mockClient.getCpf())).thenReturn(Optional.of(mockClient));
+        when(clientRepository.findByCnpj(mockClient.getCpf())).thenReturn(Optional.of(mockClient));
+
+        // Se o CPF e CNPJ forem o mesmo, mas o ID não, então o método deverá retornar True
+        assertTrue(clientService.isAlreadyRegistered(mockClient.getCpf(), mockClient.getCnpj(), 2L));
+
+        // Se o CPF, CNPJ e ID existem no banco então o método deverá retornar False
+        assertFalse(clientService.isAlreadyRegistered(mockClient.getCpf(), mockClient.getCnpj(), mockClient.getId()));
     }
 
     @Test
@@ -102,11 +151,31 @@ public class ClientServiceTests {
 
     @Test
     void testIsCpfAlreadyRegistered() {
-        // TODO add test
+        Client mockClient = new Client();
+        mockClient.setCpf("12345678910");
+        mockClient.setId(1L);
+
+        when(clientRepository.findByCpf(mockClient.getCpf())).thenReturn(Optional.of(mockClient));
+
+        // Se o CPF e ID for o mesmo o método deverá retornar false
+        assertFalse(clientService.isCpfAlreadyRegistered(mockClient.getCpf(), mockClient.getId()));
+
+        // Se o CPF for o mesmo, mas o ID não o método deverá retornar true
+        assertTrue(clientService.isCpfAlreadyRegistered(mockClient.getCpf(), 2L));
     }
 
     @Test
     void testIsCnpjAlreadyRegistered() {
-        // TODO add test
+        Client mockClient = new Client();
+        mockClient.setCnpj("01176651000108");
+        mockClient.setId(1L);
+
+        when(clientRepository.findByCnpj(mockClient.getCnpj())).thenReturn(Optional.of(mockClient));
+
+        // Se o CNPJ e ID for o mesmo o método deverá retornar false
+        assertFalse(clientService.isCnpjAlreadyRegistered(mockClient.getCnpj(), mockClient.getId()));
+
+        // Se o CNPJ for o mesmo, mas o ID não o método deverá retornar true
+        assertTrue(clientService.isCnpjAlreadyRegistered(mockClient.getCnpj(), 2L));
     }
 }
