0db7bbdf3e34323b645ac0dc52ea174dd4fe7736
Samuel Thomas <samuel.thomas1109@outlook.com>
2024-05-02 19:59:37 -0300

    Alterações nos testes dos services de cidades e estados

--- src/test/java/com/projetointegrador/projetointegrador/services/CityServiceTests.java
+++ src/test/java/com/projetointegrador/projetointegrador/services/CityServiceTests.java
@@ -1,15 +1,21 @@
 package com.projetointegrador.projetointegrador.services;
 
+import com.projetointegrador.projetointegrador.models.City;
+import com.projetointegrador.projetointegrador.models.State;
 import com.projetointegrador.projetointegrador.repositories.CityRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class CityServiceTests {
@@ -19,12 +25,27 @@ public class CityServiceTests {
     @Test
     public void testListCitiesPerState() {
         Long stateId = 1L;
+        State exampleState = new State();
+        exampleState.setId(stateId);
 
-        ResponseEntity<?> responseEntity = cityService.listCitiesPerState(stateId);
+        List<City> cities = new ArrayList<>();
+        City exampleCity1 = new City();
+        exampleCity1.setState(exampleState);
+        City exampleCity2 = new City();
+        exampleCity2.setState(exampleState);
+        cities.add(exampleCity1);
+        cities.add(exampleCity2);
 
-        assertEquals(HttpStatusCode.valueOf(200), responseEntity.getStatusCode());
+        when(cityRepository.findAll(any(Example.class))).thenReturn(cities);
 
-        Object body = responseEntity.getBody();
-        assertTrue(body instanceof List);
+        ResponseEntity<?> response = cityService.listCitiesPerState(stateId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        List<City> responseCities = (List<City>) response.getBody();
+        assert responseCities != null;
+        assertEquals(2, responseCities.size());
+
+        assertEquals(stateId, responseCities.get(0).getState().getId());
+        assertEquals(stateId, responseCities.get(1).getState().getId());
     }
 }
--- src/test/java/com/projetointegrador/projetointegrador/services/StateServiceTests.java
+++ src/test/java/com/projetointegrador/projetointegrador/services/StateServiceTests.java
@@ -1,16 +1,18 @@
 package com.projetointegrador.projetointegrador.services;
 
+import com.projetointegrador.projetointegrador.models.State;
 import com.projetointegrador.projetointegrador.repositories.StateRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class StateServiceTests {
@@ -19,11 +21,30 @@ public class StateServiceTests {
 
     @Test
     public void testListStates() {
-        ResponseEntity<?> responseEntity = stateService.listStates();
+        State exampleState1 = new State();
+        exampleState1.setId(1L);
+        State exampleState2 = new State();
+        exampleState2.setId(2L);
+        State exampleState3 = new State();
+        exampleState3.setId(3L);
 
-        assertEquals(HttpStatusCode.valueOf(200), responseEntity.getStatusCode());
+        List<State> states = new ArrayList<>();
 
-        Object body = responseEntity.getBody();
-        assertTrue(body instanceof List);
+        states.add(exampleState1);
+        states.add(exampleState2);
+        states.add(exampleState3);
+
+        when(stateRepository.findAll()).thenReturn(states);
+
+        ResponseEntity<?> response = stateService.listStates();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        List<State> responseCities = (List<State>) response.getBody();
+        assert responseCities != null;
+        assertEquals(3, responseCities.size());
+
+        assertEquals(1L, responseCities.get(0).getId());
+        assertEquals(2L, responseCities.get(1).getId());
+        assertEquals(3L, responseCities.get(2).getId());
     }
 }
