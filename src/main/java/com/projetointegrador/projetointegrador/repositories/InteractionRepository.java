c467e077fa06cefd2ed6b8bdd696c8cce2451cb9
Samuel Thomas <samuel.thomas1109@outlook.com>
2024-05-16 20:16:25 -0300

    Testes de interaction

--- src/main/java/com/projetointegrador/projetointegrador/repositories/InteractionRepository.java
+++ src/main/java/com/projetointegrador/projetointegrador/repositories/InteractionRepository.java
@@ -1,6 +1,5 @@
 package com.projetointegrador.projetointegrador.repositories;
 
-import com.projetointegrador.projetointegrador.models.City;
 import com.projetointegrador.projetointegrador.models.Interaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
