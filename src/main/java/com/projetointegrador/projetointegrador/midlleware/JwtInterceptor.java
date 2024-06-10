03773fc1af0963b141a271b7205c704e7a5f619c
Samuel Thomas <samuel.thomas1109@outlook.com>
2024-06-10 19:56:00 -0300

    Alteração no interceptor do token

--- src/main/java/com/projetointegrador/projetointegrador/midlleware/JwtInterceptor.java
+++ src/main/java/com/projetointegrador/projetointegrador/midlleware/JwtInterceptor.java
@@ -1,11 +1,17 @@
 package com.projetointegrador.projetointegrador.midlleware;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projetointegrador.projetointegrador.utils.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
@@ -20,13 +26,25 @@ public class JwtInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
+
+        System.out.println(token);
+
         if (token == null || !jwtUtils.isValidToken(token)) {
-            response.sendRedirect("/login");
+            setJsonResponse(response, HttpStatus.UNAUTHORIZED, "Token inválido ou faltante");
             return false;
         } else if (jwtUtils.isTokenExpired(token)) {
-            response.sendRedirect("/login");
+            setJsonResponse(response, HttpStatus.UNAUTHORIZED, "Token expirado");
             return false;
         }
         return true;
+    }
+
+    private void setJsonResponse(HttpServletResponse response, HttpStatus status, String message) throws IOException {
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", message);
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(errorResponse));
     }
 }
